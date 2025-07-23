96527c29de8d635dd352a9561a02884dfe8b11e8
Moizkazmi245 <abdul.moiz.kazmi@gmail.com>
2025-07-23 23:59:00 +0500

    done

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const lightboxImg = document.getElementById("lightbox-img");
 const galleryItems = document.querySelectorAll(".gallery-item img");
 const closeBtn = document.querySelector(".close");
 
+// Open lightbox on image click
 galleryItems.forEach((img) => {
   img.addEventListener("click", () => {
     lightbox.style.display = "flex";
@@ -10,12 +11,21 @@ galleryItems.forEach((img) => {
   });
 });
 
+// Close when clicking X button
 closeBtn.addEventListener("click", () => {
   lightbox.style.display = "none";
 });
 
+// Close when clicking outside image
 lightbox.addEventListener("click", (e) => {
   if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
+
+// Close when pressing Escape key
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
     lightbox.style.display = "none";
   }
 });
